e5bb25c96ca6c332301947dedc5192857bb6c112
Dharshan S Kotian <dcodecraft@gmail.com>
2023-09-09 19:38:23 +0530

    python anywhere

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4*cb*unwmd3r$3!#647^-@s$@noe4o4r=^3+zv%de3$p^^o@_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,6 +129,3 @@ STATICFILES_DIRS =  [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR , 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build', 'static')
