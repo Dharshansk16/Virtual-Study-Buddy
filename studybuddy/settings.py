dd21f8b4f533586a0c630cd49f97e4e071e96937
Dharshan S Kotian <dcodecraft@gmail.com>
2023-10-02 11:59:15 +0530

    Vercel-commit

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4*cb*unwmd3r$3!#647^-@s$@noe4o4r=^3+zv%de3$p^^o@_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'studybuddy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS =  [
     BASE_DIR / 'static',
--- studybuddy/wsgi.py
+++ studybuddy/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybuddy.settings')
 
 application = get_wsgi_application()
 
-
+app = application
